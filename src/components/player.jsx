995403f13c9f802b53a7a6f63f8951bf45f6d87c
P.shivakumar <60265650+shivakumar1998@users.noreply.github.com>
2023-12-05 11:17:53 +0530

    session_76

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -7,9 +7,11 @@ export default function Player({ name, symbol }) {
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  //let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    //btnCaption = "Save";
   }
 
   return (
@@ -18,7 +20,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
